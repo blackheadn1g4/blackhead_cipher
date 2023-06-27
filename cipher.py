76cd063027fccd93029bfc4c3137dee4b78e57a6
blackheadn1ga <106983176+blackheadn1g4@users.noreply.github.com>
2023-06-27 14:47:10 +0400

    cipher.py

--- cipher.py
+++ cipher.py
@@ -1,4 +1,3 @@
-import json
 import colorama
 from colorama import Fore
 from pystyle import *
@@ -63,37 +62,133 @@ def crypt(task, sentence, keys) -> str:
 
 
 def main():
-    with open("keys.json", "r") as f:
-        keys = json.load(f)
+    keys = {
+        "a": "e",
+        "b": "a",
+        "c": "m",
+        "d": "c",
+        "e": "d",
+        "f": "b",
+        "g": "h",
+        "h": "i",
+        "i": "f",
+        "j": "o",
+        "k": "g",
+        "l": "r",
+        "m": "j",
+        "n": "l",
+        "o": "n",
+        "p": "s",
+        "q": "q",
+        "r": "w",
+        "s": "t",
+        "t": "v",
+        "u": "u",
+        "v": "z",
+        "w": "k",
+        "x": "p",
+        "y": "x",
+        "z": "y",
+        "A": "Y",
+        "B": "X",
+        "C": "P",
+        "D": "K",
+        "E": "Z",
+        "F": "U",
+        "G": "V",
+        "H": "T",
+        "I": "W",
+        "J": "Q",
+        "K": "S",
+        "L": "N",
+        "M": "L",
+        "N": "J",
+        "O": "R",
+        "P": "G",
+        "Q": "O",
+        "R": "F",
+        "S": "I",
+        "T": "H",
+        "U": "B",
+        "V": "D",
+        "W": "C",
+        "X": "M",
+        "Y": "A",
+        "Z": "E",
+        "1": "8",
+        "2": "0",
+        "3": "7",
+        "4": "9",
+        "5": "2",
+        "6": "4",
+        "7": "5",
+        "8": "3",
+        "9": "1",
+        "0": "6",
+        "-": "~",
+        "+": "¬",
+        "=": "`",
+        " ": "£",
+        ",": ">",
+        ";": "<",
+        ":": "?",
+        "'": "/",
+        "%": "}",
+        "#": "{",
+        "!": "]",
+        "$": "[",
+        "(": "_",
+        ")": "*",
+        "&": "&",
+        "*": ")",
+        "_": "(",
+        "[": "$",
+        "]": "!",
+        "{": "#",
+        "}": "%",
+        "/": "'",
+        "?": ":",
+        "<": ";",
+        ">": ",",
+        "£": " ",
+        "`": "=",
+        "¬": "+",
+        "~": "-"
+    }
 
     active = True
 
     while active:
-        print("[1] Encrypting text.")
-        print("[2] Decrypting text.")
-
+        print(f"{Fore.WHITE}[{Fore.MAGENTA}1{Fore.WHITE}] {Fore.LIGHTGREEN_EX}Encrypting text.")
+        print(f"{Fore.WHITE}[{Fore.MAGENTA}2{Fore.WHITE}] {Fore.LIGHTGREEN_EX}Decrypting text.")
+        print("")
+        
         try:
-            usr_input = int(input(">>> "))
+            usr_input = int(input(f"{Fore.WHITE}[{Fore.MAGENTA}>>>{Fore.WHITE}] "))
         except ValueError:
             continue
 
         if usr_input == 1:
-            sentence = str(input("\nEnter sentence >>> "))
+            print(f"\n{Fore.LIGHTGREEN_EX}Enter sentence!")
+            sentence = str(input(f"{Fore.WHITE}[{Fore.MAGENTA}>>>{Fore.WHITE}] "))
             encryption = crypt("encrypt", sentence, keys)
 
-            print("\n========================ENCRYPTED TEXT===========================\n")
+            print(f"\n{Fore.LIGHTGREEN_EX}======================== {Fore.MAGENTA}ENCRYPTED TEXT {Fore.LIGHTGREEN_EX}===========================\n")
             print(encryption)
-            print("\n=================================================================\n")
+            print("\n===================================================================\n")
 
         elif usr_input == 2:
-            sentence = str(input("\n Enter sentence >>> "))
+            print(f"\n{Fore.LIGHTGREEN_EX}Enter sentence!")
+            sentence = str(input(f"{Fore.WHITE}[{Fore.MAGENTA}>>>{Fore.WHITE}] "))
             decryption = crypt("decrypt", sentence, keys)
 
-            print("\n========================DECRYPTED TEXT===========================\n")
+            print(f"\n{Fore.LIGHTGREEN_EX}======================== {Fore.MAGENTA}DECRYPTED TEXT {Fore.LIGHTGREEN_EX}===========================\n")
             print(decryption)
-            print("\n=================================================================\n")
-
-        exit_stay = str(input("If you want to exit, type 'x'. If not and you would like to continue, press 'Enter' key >>> "))
+            print("\n===================================================================\n")
+            print("")
+            print(f"{Fore.LIGHTGREEN_EX}If you want to {Fore.RED}exit{Fore.LIGHTGREEN_EX}, {Fore.LIGHTGREEN_EX}type {Fore.LIGHTGREEN_EX}'{Fore.MAGENTA}x{Fore.LIGHTGREEN_EX}'. {Fore.LIGHTGREEN_EX}If not and you would like to continue, press {Fore.LIGHTGREEN_EX}'{Fore.MAGENTA}Enter{Fore.LIGHTGREEN_EX}' {Fore.LIGHTGREEN_EX}key.")     
+            print("")
+        exit_stay = str(input(f"{Fore.WHITE}[{Fore.MAGENTA}>>>{Fore.WHITE}] "))
 
         if exit_stay == "x":
             active = False
@@ -101,3 +196,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
